2a9b769e15106f6531645c2b39b6414358645143
perdonamedietaburger <perdonamedietaburger@gmail.com>
2025-11-12 18:59:52 -0300

    Update next.config.js

--- perdonamedieta_next/next.config.js
+++ perdonamedieta_next/next.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
+  experimental: { },
   images: { unoptimized: true },
 }
 module.exports = nextConfig
